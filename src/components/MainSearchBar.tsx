fcfa6f0a3cd4d83f279d82c75acf35b8afe905bc
This-Is-Prince <this.is.kumar.prince@gmail.com>
2022-05-12 18:14:05 +0530

    Main search bar background image static assets added

--- src/components/MainSearchBar.tsx
+++ src/components/MainSearchBar.tsx
@@ -3,14 +3,8 @@ import { UserContext } from "../app/context";
 
 const MainSearchBar = () => {
   const { appState } = useContext(UserContext)!;
-  const image = appState.mainBgImage!;
   return (
-    <section
-      className="main-search-bar"
-      style={{
-        backgroundImage: `url(${image.links.download})`,
-      }}
-    >
+    <section className="main-search-bar">
       <h1 className="heading">Download High Quality Images by creators</h1>
       <p className="desc">
         Over 2.4 million+ stock Images by our talented community
