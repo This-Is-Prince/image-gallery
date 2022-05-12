a8365d3fed011d35acdad343a6216d830d43857e
This-Is-Prince <this.is.kumar.prince@gmail.com>
2022-05-12 18:56:32 +0530

    navbar search completed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,45 @@
+import { useEffect, useRef } from "react";
 import { AiOutlineSearch } from "react-icons/ai";
 import { TiDelete } from "react-icons/ti";
 
 const Navbar = () => {
+  const clearIcon = useRef<HTMLButtonElement | null>(null);
+  const searchBar = useRef<HTMLInputElement | null>(null);
+
+  useEffect(() => {
+    if (searchBar !== null && searchBar.current !== null) {
+      searchBar.current.addEventListener("keyup", function (e) {
+        if (clearIcon && clearIcon.current) {
+          if (this.value && clearIcon.current.style.visibility != "visible") {
+            clearIcon.current.style.visibility = "visible";
+          } else if (!this.value) {
+            clearIcon.current.style.visibility = "hidden";
+          }
+        }
+      });
+    }
+    if (clearIcon !== null && clearIcon.current !== null) {
+      clearIcon.current.addEventListener("click", function (e) {
+        if (searchBar && searchBar.current) {
+          searchBar.current.value = "";
+        }
+        this.style.visibility = "hidden";
+      });
+    }
+  }, [clearIcon, searchBar]);
   return (
     <nav className="navbar">
       <div className="wrapper">
         <button className="search-btn search-icon">
           <AiOutlineSearch />
         </button>
-        <input className="search" placeholder="Search" type="text" />
-        <button className="search-btn clear-icon">
+        <input
+          ref={searchBar}
+          className="search"
+          placeholder="Search"
+          type="text"
+        />
+        <button ref={clearIcon} className="search-btn clear-icon">
           <TiDelete />
         </button>
       </div>
