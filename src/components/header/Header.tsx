6b95c2fc835625b9817bd25a0cda85dea5622818
This-Is-Prince <this.is.kumar.prince@gmail.com>
2022-08-01 08:40:03 +0530

    feat(Header): made header sticky

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,6 +1,6 @@
 const Header = () => {
   return (
-    <header className="shadow-md p-5">
+    <header className="shadow-md p-5 sticky top-0 bg-white z-50">
       <h1 className="font-bold text-2xl font-mono">Image Gallery</h1>
     </header>
   );
